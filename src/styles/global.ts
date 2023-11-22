68ff16314ab0f26343843a496981ff5fe2ee97c2
Tatiana Tavares <tatianatavares@Tatianas-MacBook-Pro.local>
2023-11-22 14:33:17 +0000

    Added global styling

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -23,11 +23,28 @@ export const GlobalStyle = createGlobalStyle `
    box-sizing : border-box;
 }
 
+html {
+   @media (max-width: 1080px) {
+      font-size: 93.75%
+   }
 
+   @media (max-width: 720px) {
+      font-size: 87.5%
+   }
+
+}
 
 body {
    background: var(--background);
    -webkit-font-smoothing: antialiased;
+}
+
+body, input, textarea, button {
+   font-family: 'Poppins', sans-serif;
+   font-weight:400;
+}
+h1, h2, h3, h4, h5, h6, strong{
+   font-weight: 600;
 }
 
 button {
